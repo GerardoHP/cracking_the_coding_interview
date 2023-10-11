7a86e245d7ed1c0ac38fb2bda42bf608d95625e3
Gerardo de Jesús Hernández Perales <gerardojhp@gmail.com>
2023-10-11 16:23:55 -0600

    Develop (#6)
    * add command style execution for the cli utility
    
    * add check permutation to cli
    
    * add support for Docker
    
    * add urlify exercise
    
    ---------
    
    Co-authored-by: Gerardo Hernandez <gerardo@hernandez.perales.dev>

---
+++ pkg/chapter_01/03_urlify_test.go
@@ -0,0 +1,16 @@
+package chapter_01_test
+
+import (
+	"github.com/GerardoHP/cracking_the_coding_interview/pkg/chapter_01"
+	"testing"
+)
+
+func TestUrlify(t *testing.T) {
+	expected := "Mr%20John%20Smith"
+	str := "Mr John Smith    "
+	i := 13
+	r := chapter_01.Urlify([]rune(str), i)
+	if expected != r {
+		t.Fatalf("expected %v, but found %v \n", expected, r)
+	}
+}
